92247678909ac8d4f82a3cfe3da9bf9a216dfc22
Iva1010 <contact@iva-g.cc>
2021-06-29 19:54:25 +0700

    add node and node init

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <cstddef>
+#include <stdio.h>
+#include <malloc.h>
+
+struct node {
+    void *data;
+    struct node *pnt_next;
+    struct node *pnt_prev; 
+};
+
+
+//Node init
+struct node *init(int f_data) {
+    struct node *root;
+    root = (struct node*)malloc(sizeof(struct node));
+    root -> data = f_data;
+    root -> pnt_next = NULL;
+    root -> pnt_prev = NULL;
+    return (root);
+}
+
+
+int main() {
+
+}
