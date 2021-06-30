09991670f7619914655d648316f3de9f10ef029a
Iva1010 <contact@iva-g.cc>
2021-06-30 14:21:09 +0700

    add insert_end

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h>
+
 typedef struct Node {
     int x;
     struct Node* next;
 } Node;
 
-int main(void){
-    Node root;
-    root.x = 12;
-    root.next = malloc(sizeof(Node));
-    root.next->x = -23;
-    root.next->next = malloc(sizeof(Node));
-    root.next->next->x = 2323;
-    root.next->next->next = NULL;
-
-    printf("First = %d\n", root.x);
-    printf("Second = %d\n", root.next->x);
-
-    Node* curr = &root;
+void insert_end(Node** root, int value) {
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL){
+        exit(1);
+    }
+    new_node->next = NULL;
+    new_node->x = value;
     
+    if (*root == NULL) {
+        *root = new_node;
+        return;
+    }
+
+    Node* curr = *root;
+    while (curr->next != NULL) {
+        curr = curr->next;    
+    }
+    curr->next = new_node;
+}
+
+int main(void){
+    Node* root = NULL;
+
+    insert_end(&root, -2);
+    insert_end(&root, 23);
+    
+    Node* curr = root;
     while (curr != NULL) {
         printf("%d\n", curr -> x);
         curr = curr->next;
     }
-    free(root.next->next); 
-    free(root.next);    
+
     return 0;
 }
